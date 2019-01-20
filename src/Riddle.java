b9ac7cdb9cc59a9871e1d276fb0f2f7de79bd6cf
audreychan <league@WTS5.attlocal.net>
2019-01-20 11:27:16 -0800

    Riddle

--- src/Riddle.java
+++ src/Riddle.java
@@ -1,3 +1,4 @@
+import java.awt.Dimension;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -15,6 +16,11 @@ public class Riddle implements MouseListener {
 	JButton hint = new JButton("Hint");
 	JTextField output = new JTextField();
 	
+	public static void main(String[] args) {
+		Riddle riddle = new Riddle();
+		riddle.setup();
+	}
+	
 	public void setup() {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,6 +33,10 @@ public class Riddle implements MouseListener {
 		
 		submit.addMouseListener(this);
 		hint.addMouseListener(this);
+		
+		label.setText("A boy was at a carnival and went to a booth where a man said to the boy, \"If I write your exact weight on this piece of paper then you have to give me $50, but if I cannot, I will pay you $50.\" The boy looked around and saw no scale so he agrees, thinking no matter what the carny writes he'll just say he weighs more or less. In the end the boy ended up paying the man $50. What did the man write?");
+		
+		frame.setPreferredSize(new Dimension (500, 500));
 	}
 
 	@Override
@@ -58,4 +68,16 @@ public class Riddle implements MouseListener {
 		// TODO Auto-generated method stub
 		
 	}
+	/*Q: A boy was at a carnival and went to a booth 
+	*where a man said to the boy, "If I write your 
+	*exact weight on this piece of paper then you 
+	*have to give me $50, but if I cannot, I will 
+	*pay you $50." The boy looked around and saw 
+	*no scale so he agrees, thinking no matter what 
+	*the carny writes he'll just say he weighs 
+	*more or less. In the end the boy ended up paying 
+	*the man $50. What did the man write?
+	*A: "Your exact weight"
+	*H: He wrote exactly what he said he would
+	*/
 }
